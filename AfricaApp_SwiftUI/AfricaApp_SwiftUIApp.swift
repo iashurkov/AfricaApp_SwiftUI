fa49bdf90b836e450c00cb53791d9b0ed3b0fb4e
iashurkov <iashurkov@uma.tech>
2022-11-27 22:33:53 +0300

    Initial Commit

---
+++ AfricaApp_SwiftUI/AfricaApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp_SwiftUIApp.swift
+//  AfricaApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
